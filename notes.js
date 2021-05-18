49d1bf8f0fa0aee0efaa329311335dbe54661ccb
Anna <CodeWithAnna@gmail.com>
2021-05-18 15:12:30 +0530

    Commit

--- notes.js
+++ notes.js
@@ -1,1 +1,18 @@
-console.log('Note making App');
+//eventsListner
+//  if user adds a note, adding it to the localStorage
+
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("click", function(e){
+  let addTxt = document.getElementById("addTxt");
+  let notes =  localStorage.getItem("notes");
+  if(notes==null){
+      notesObj = [];
+  }
+  else{
+      notesObj = JSON.parse(notes)
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  console.log(notesObj);
+})
